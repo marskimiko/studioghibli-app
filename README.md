4e76c3d0ceec9d338236cdccc2a9dcf7c2cb2971
marisasaunders <marisaksaunders@gmail.com>
2022-03-07 12:55:20 -0500

    added list code

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 // description of app 
 -app is a database of schools in the UK
 -you can click on the 'All Universities' button and it will reveal all of the schools to you
+-event listeners are DOMContentLoaded, click, and 
 
 // url of repo 
 
--- index.js
+++ index.js
@@ -1,29 +1,109 @@
-const BASE_URL = 'http://universities.hipolabs.com/search?country=United+Kingdom'
+const fetchLink = 'https://ghibliapi.herokuapp.com/films'
+let addFilm = false;
+
+// grabs ul where im gonna put the schools once I have them
+//const form = document.querySelector('.un');
+//const ul = document.getElementById('film-list');
+
+const addBtn = document.querySelector("#new-film-btn");
+addBtn.addEventListener("click", addFilmButton)
 
 window.addEventListener('DOMContentLoaded', () => {
-  getSchools()
+  getFilms()
+  document.getElementById('films').addEventListener('click', getFilms)
+
+  document.querySelector('.add-film-form').addEventListener('submit', handleSubmit)
+  
+  //form.addEventListener('submit', handleSubmit)
 })
 
-function getSchools() {
-  // grabs ul where im gonna put the schools once I have them
-  const ul = document.getElementById('univeristy-list')
-  
+function getFilms() {
+  const ul = document.getElementById('film-list');
+  const info = document.getElementById('info')
+  info.innerHTML = ''
+  ul.innerHTML = ''
   // only need one perameter for fetch bc defaults to send an HTTP GET request
   // fetch instantaneoulsy returns a promise object
   // promise object statuses: pending, fulfilled, rejected
   // fetch is asynchornus execution (allows execution to continute while fetch is being worked on) 
-  fetch(BASE_URL)
+  fetch(fetchLink)
   .then(res => res.json())
   .then(data => {
     // iterates over Lis to give me an array of the Lis
-    data.forEach(school => {
+    // data-id adss id as a key in the dataset 
+    data.forEach(film => {
       // go through each show, make an li and add it (append it) to the innerHTML of the ul
       ul.innerHTML += `
-      <li>${school.name}</li>
+      <li>
+      <a href="#" data-id="${film.id}">${film.title}</a>
+      </li>
       
       `
       
     })
+    attachClicksToLinks()
   })
+ 
+}
+
+const attachClicksToLinks = () => {
+  const films = document.querySelectorAll('a')
+  films.forEach((film) => {
+    film.addEventListener('click', showFilm)
+  })
+}
+
+const showFilm = (event) => {
+  console.log(event.target.dataset.id)
+  const info = document.getElementById('info')
+  const ul = document.getElementById('film-list')
+  ul.innerHTML = ''
+  fetch(fetchLink + `/${event.target.dataset.id}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    info.innerHTML = `<h1>${data.title}</h1>
+<h3>Description:</h3>
+<p>${data.description}</p>
+<h3>Realse Date:</h3>
+<p>${data.release_date}</p>
+<h3>Running Time:</h3>
+<p>${data.running_time}</p>
+<h3>Rotten Tomato:</h3>
+<p>${data.rt_score}%</p>`
+  })
+}
+
+function addFilmButton() {
+  const filmFormContainer = document.querySelector(".container");
+  addFilm = !addFilm;
+    if (addFilm) {
+      filmFormContainer.style.display = "block";
+    } else {
+      filmFormContainer.style.display = "none";
+    }
+} 
+
+function handleSubmit(event){
+  //getFilms()
+  const ul = document.getElementById('film-list');
+  // prevent reloading of page preventDefault
+  event.preventDefault();
+
+  // get the input value
+  const film = document.querySelector("#new-film-description");
+  
+  // one you click on submit, do things with input value
+  // extract the value of the input
+
+  // create new list element
+  const newFilm = document.createElement('li');
+  newFilm.textContent = film.value;
+
+
+  ul.append(newFilm);
+
+  // clear the input field
+  film.value = ''
 
 }
